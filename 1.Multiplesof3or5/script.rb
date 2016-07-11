875741c028bf738cc2f7845b7f1b7749c8458e04
Lucasrpmv <Lucasrpmv@hotmail.com>
2016-07-10 19:35:50 -0700

    Resolver

---
+++ 1.Multiplesof3or5/script.rb
@@ -0,0 +1,8 @@
+total = 0
+for i in 1...1000 do					#All numbers from 1 to 1000 stored in i
+	if ((i%3 == 0) || (i%5 == 0))		#Argument to take every multiple of 3 or 5
+		total += i 						#Add each to total
+	end
+end
+
+puts "The total sum of 5 or 3 multiples below 1000 is: #{total}."
